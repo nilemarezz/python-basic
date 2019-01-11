99e70288750c47f01dcdb503c5444be0dfcb2ee8
Unknown <nilenon@gmail.com>
2019-01-12 00:19:33 +0700

    [Add] If,else file

---
+++ If,else.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+a= 10
+b=10
+if(a>b):
+    print('a is bigger than b')
+
+elif(a<b):
+    print('b is bigger than a')
+    
+else: print('a is equal to b ')
+
+
+# In[13]:
+
+
+a = 10
+b = 20
+if(a<12):
+    if(a>9):
+        print('a is between 10-11')
+    else:
+        print('a is lessthan 12')
+else:
+    print('a is more than 12')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
