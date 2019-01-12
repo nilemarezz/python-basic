3b1f40591177b674b538110916c309f347572635
Unknown <nilenon@gmail.com>
2019-01-12 10:52:34 +0700

    [Add] Exercise calGrade

---
+++ Exercise Calculate Grade.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# โจทย์ คำนวนgrade โดยใช้ if else 
+
+# In[10]:
+
+
+grade = 1000
+if(grade <= 50):
+    print('F')
+elif(grade> 50 and grade <= 60):
+    print('D')
+elif(grade > 60 and grade <= 70):
+    print('C')
+elif(grade > 70 and grade <= 80):
+    print('B')
+elif(grade > 80 and grade <= 100):
+    print('A')
+else:
+    print('Invalid Input')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
